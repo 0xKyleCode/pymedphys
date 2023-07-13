a43159bbbfdb1d59af90eba263e5b9cd293efdf2
Kyle Bromma <kbromma@gmail.com>
2023-07-13 16:37:04 -0700

    Update git so it merges with branch only

--- scripts/update-git.py
+++ scripts/update-git.py
@@ -5,17 +5,8 @@ def merge_main_branch():
     # Open the repository
     repo = Repo(".")
 
-    # Get the current branch
-    current_branch = repo.active_branch
-
-    # Switch to the main branch
-    repo.git.checkout("main")
-
     # Merge the main branch into the current branch
-    repo.git.merge(current_branch.name)
-
-    # Switch back to the current branch
-    repo.git.checkout(current_branch)
+    repo.git.merge("main")
 
     print("Main branch merged into the current branch.")
 
